cb35ace4ba500ebb63814825696af59bfb35fb67
Chanatip Deemee <78657820+Loxyez@users.noreply.github.com>
2023-03-17 22:03:01 +0700

    Update main.dart
    + Functional Setting Page
    + GPS Tracking
    + Lat, Long Position Update

--- velocityvision/lib/main.dart
+++ velocityvision/lib/main.dart
@@ -34,7 +34,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  double _speed = 0.0;
+  Color _gaugeColor = Colors.blue;
+  Color _themeColor = Colors.blue;
+  double _speed = 0;
   Position? _lastPosition;
   StreamSubscription<Position>? _positionStreamSubscription;
   Timer? _timer;
@@ -43,6 +45,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   double _latitude = 37.7749;
   double _longitude = -122.4194;
+
+  bool _useCustomerColors = false;
+  List<double> _alertSpeeds = [90, 120, 180];
+  List<Color> _alertColors = [Color.fromARGB(255, 0, 255, 55), Color.fromARGB(255, 255, 204, 0), Colors.red];
+
+  Color _primaryColor = Colors.blue;
+  Color _accentColor = Colors.blueAccent;
 
   @override
   void initState() {
@@ -84,38 +93,158 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+  return DefaultTabController(
+    length: 2,
+    child: Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text(widget.title!),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: 400,
-              height: 400,
-              padding: EdgeInsets.all(10),
-              child: KdGaugeView(
-                minSpeed: 0,
-                maxSpeed: 260,
-                speed: _speed,
-                animate: true,
-                duration: Duration(seconds: 5),
-                alertSpeedArray: [90, 120, 180],
-                alertColorArray: [Color.fromARGB(255, 0, 255, 55), Color.fromARGB(255, 255, 204, 0), Colors.red],
-                unitOfMeasurement: "km/h",
-                gaugeWidth: 20,
-                fractionDigits: 1,
-              ),
-            ),
-            SizedBox(height: 20),
-            Text("Latitude: ${_lastPosition?.latitude ?? _latitude}"),
-            Text("Longitude: ${_lastPosition?.longitude ?? _longitude}"),
+        bottom: TabBar(
+          tabs: [
+            Tab(text: "Speedometer"),
+            Tab(text: "Settings"),
           ],
         ),
       ),
-    );
-  }
+      body: TabBarView(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 400,
+                  height: 400,
+                  padding: EdgeInsets.all(10),
+                  child: KdGaugeView(
+                    minSpeed: 0,
+                    maxSpeed: 260,
+                    speed: _speed,
+                    animate: true,
+                    duration: Duration(seconds: 5),
+                    alertSpeedArray: [90, 120, 180],
+                    alertColorArray: [Color.fromARGB(255, 0, 255, 55), Color.fromARGB(255, 255, 204, 0), Colors.red],
+                    unitOfMeasurement: "km/h",
+                    gaugeWidth: 20,
+                    fractionDigits: 1,
+                    speedTextStyle: TextStyle(
+                      color: _gaugeColor,
+                      fontSize: 50,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text("Latitude: ${_lastPosition?.latitude ?? _latitude}"),
+                Text("Longitude: ${_lastPosition?.longitude ?? _longitude}"),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Gauge color"),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Expanded(
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _gaugeColor = Colors.blue;
+                          });
+                        },
+                        child: Container(
+                          height: 50,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _gaugeColor = Colors.red;
+                          });
+                        },
+                        child: Container(
+                          height: 50,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _gaugeColor = Colors.green;
+                          });
+                        },
+                        child: Container(
+                          height: 50,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Text("Theme color"),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Expanded(
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _themeColor = Colors.blue;
+                          });
+                        },
+                        child: Container(
+                          height: 50,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _themeColor = Colors.red;
+                          });
+                        },
+                        child: Container(
+                          height: 50,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _themeColor = Colors.green;
+                          });
+                        },
+                        child: Container(
+                          height: 50,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
 }
